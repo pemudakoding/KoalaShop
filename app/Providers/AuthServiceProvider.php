9834d2a6419a184e7d530950f53ab21ec1465c79
pemudakoding <pemudakoding@gmail.com>
2022-01-13 19:09:05 +0800

    modified: move User Address Policy to outside User Folder

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,7 +5,7 @@ namespace App\Providers;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
 use App\Models\{UserAddress};
-use App\Policies\User\{UserAddressPolicy};
+use App\Policies\{UserAddressPolicy};
 
 class AuthServiceProvider extends ServiceProvider
 {
